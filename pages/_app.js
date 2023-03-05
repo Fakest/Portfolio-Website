3eb95643ea0749ab2a0db4bee3a437eb056165ab
Harry Beggs <harrybeggs@harrys-mbp.home>
2023-03-05 01:35:12 +0000

    Analytics addition

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import { ChakraProvider } from "@chakra-ui/react"
 import Layout from '../components/layouts/main'
 import theme from '../libs/theme'
 import { Roboto } from '@next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 
 const font = Roboto({ subsets: ['latin'], weight:'300' })
 
 const Website = ({Component, pageProps, router}) => {
     return(
-        <main className={font.className}>
+        <main>
+            <Analytics />
             <ChakraProvider theme={theme}>
                 <Layout router={router}>
                     <Component {...pageProps} key={router.route} />
