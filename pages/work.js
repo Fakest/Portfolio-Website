b93a76c478ba93d8ebc3134d7f0cafc5f12d3250
Harry Beggs <hjbeggs99@gmail.com>
2023-04-25 14:16:27 +0100

    Test new works

--- pages/work.js
+++ pages/work.js
@@ -1,8 +1,9 @@
 import { Container, Heading, SimpleGrid, Divider } from "@chakra-ui/react";
 import Section from "../components/section";
-import { WorkGridItem } from "../components/grid-item";
+import { WorkGridItem, GridItem } from "../components/grid-item";
 
 import vp from '../public/images/vp.jpg'
+import emir from '../public/images/emir.jpg'
 
 const Work = () => {
     return (
@@ -12,13 +13,13 @@ const Work = () => {
             </Heading>
             <SimpleGrid columns={[1,1,2]} gap={6}>
                 <Section>
-                    <WorkGridItem id="VP" title="Vanishing Point Detection" thumbnail={vp} href="https://github.com/Fakest/VPDetection">
+                    <GridItem id="VP" title="Vanishing Point Detection" thumbnail={vp} href="https://github.com/Fakest/VPDetection">
                         Submitted as my disertation to my Honours Degree, analysis on different vanishing point detection methodologies and how they can be used and improved upon.
-                    </WorkGridItem>
+                    </GridItem>
                 </Section>
                 <Section>
-                <WorkGridItem id="VP" title="Vanishing Point Detection" thumbnail={vp}>
-                        Submitted as my disertation to my Honours Degree, analysis on different vanishing point detection methodologies and how they can be used and improved upon.
+                <WorkGridItem id="MS" title="Global Transaction Reporting" thumbnail={emir}>
+                        While working at Morgan Stanley I have worked with a fantastic team to help build Morgan Stanleys Global Transaction Reporting System, specifically regarding EMIR regulation.
                     </WorkGridItem>
                 </Section>
                 <Section>
